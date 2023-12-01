ad7c04721330964ccc405652b1d0dfe793b27b77
gustavo <gustavo.isaac@gmail.com>
2023-12-01 10:40:20 +0000

    Post

---
+++ app/src/main/java/com/artemissoftware/mockauthentication/data/remote/dto/Post.kt
@@ -0,0 +1,14 @@
+package com.artemissoftware.mockauthentication.data.remote.dto
+
+import com.squareup.moshi.Json
+
+data class Post(
+    @field:Json(name = "userId")
+    val userId: Int,
+    @field:Json(name = "id")
+    val id: Int,
+    @field:Json(name = "title")
+    val title: String,
+    @field:Json(name = "body")
+    val body: String,
+)
